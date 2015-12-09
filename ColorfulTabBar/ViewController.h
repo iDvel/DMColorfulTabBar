003ae6886fcdab8be2eef86a167d91523a604a48
Dvel <paldvel@qq.com>
2015-12-09 14:43:49 +0800

    Initial Commit

---
+++ ColorfulTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorfulTabBar
+//
+//  Created by Dvel on 15/12/9.
+//  Copyright © 2015年 Dvel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ColorfulTabBar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorfulTabBar
+//
+//  Created by Dvel on 15/12/9.
+//  Copyright © 2015年 Dvel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
